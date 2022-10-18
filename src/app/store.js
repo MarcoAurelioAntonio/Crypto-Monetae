dfccc9707718eb455ce55240dfa59fd55502aecb
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-10-18 00:33:20 -0300

    setup workingtree and linters

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    /* counter: counterReducer, */
   },
 });
+
+export default store;
